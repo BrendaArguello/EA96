0c0ae3d7bb7f5da5a318fe7a801835257a24fb77
BrendaArguello <brendiarguello@gmail.com>
2025-01-14 21:09:29 -0600

    Practica5

---
+++ Practica5.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(List<Integer> candies, int extraCandies) {
+
+        List<Boolean>result=newArrayList();
+        int greatest=0;
+        for(int  i=0; i < candies.size(); i++){
+            if(greatest<=candies.get(i)){
+                greatest=candies.get(i);
+            }
+        }
+
+        for(int i=0; i< candies.size(); i++){
+            result.add(candies.get(i)+extraCandies>=greatest);
+        }
+        return result;
+    }
+}
